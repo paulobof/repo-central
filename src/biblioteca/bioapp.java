9f36c29375fc82c0014202ec3532112df32e169c
paulobof <paulobof@gmail.com>
2021-09-03 12:09:16 -0300

    Revert "Criada a classe emprestimo"
    This reverts commit d9c5c48e738098fd473161dbd48af2167acad436.

--- src/biblioteca/bioapp.java
+++ src/biblioteca/bioapp.java
@@ -5,9 +5,6 @@ public class bioapp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Livro l = new Livro();
-		Usuario u = new Usuario();
-		
-		Emprestimo emprestimo = new Emprestimo();
 	}
 
 }
